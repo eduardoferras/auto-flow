7aed0c9b52d7be2cad28407486f5162d0f3d6590
Eduardo Ferras <edferras@gmail.com>
2025-12-24 18:48:54 -0300

    chore(web): replace fonts with inter font primary

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "@styles/globals.scss";
+import { inter } from "@assets/fonts";
 import AppHeader from "@components/AppHeader";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Auto Flow",
@@ -25,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={inter.variable}>
         <AppHeader />
         {children}
       </body>
