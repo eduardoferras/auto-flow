12133274e9f80536a4850c03d6b4745a139af877
Eduardo Ferras <edferras@gmail.com>
2025-12-24 15:15:45 -0300

    add main.scss prependData to next.config.ts and globals.scss to layout.tsx

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "@styles/main.scss";
+import "@styles/globals.scss";
+import AppHeader from "@components/AppHeader";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <AppHeader />
         {children}
       </body>
     </html>
