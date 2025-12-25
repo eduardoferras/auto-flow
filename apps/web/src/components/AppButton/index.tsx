08b4ba0b281bd2c97be545394f7684b0a1fa8d9c
Eduardo Ferras <edferras@gmail.com>
2025-12-25 02:48:03 -0300

    refactor(web): improve typing and merge className/style to prevent override to AppButton

--- apps/web/src/components/AppButton/index.tsx
+++ apps/web/src/components/AppButton/index.tsx
@@ -1,19 +1,27 @@
 import type AppButtonProps from "@interfaces/AppButtonProps";
 import type React from "react";
 import S from "./styles.module.scss";
+
 export default function AppButton({
   children,
   type = "button",
   variant = "plain",
   color,
-  ...props
+  className,
+  style,
+  ...rest
 }: Readonly<AppButtonProps> & React.ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button
-      className={`${S.button} ${S[`button--${variant}`]}`}
+      className={`${S.button} ${S[`button--${variant}`]} ${className ?? ""}`}
       type={type}
-      style={{ "--btn-color": color } as React.CSSProperties}
-      {...props}
+      style={
+        {
+          "--btn-color": color,
+          ...style,
+        } as React.CSSProperties
+      }
+      {...rest}
     >
       {children}
     </button>
--- apps/web/src/interfaces/AppButtonProps.ts
+++ apps/web/src/interfaces/AppButtonProps.ts
@@ -1,6 +1,4 @@
 export default interface AppButtonProps {
-  children: React.ReactNode;
-  type?: "button" | "submit" | "reset";
   variant?: "plain";
   color?: string;
 }
