a9134e77d75d1cb80a2e3f2aaa9dc9565c854fdd
Eduardo Ferras <edferras@gmail.com>
2025-12-25 20:13:03 -0300

    feat(web): add AppFooter component (#27)
    * feat(web): add AppLogo component
    
    * refactor(web): replace logo with AppLogo
    
    * refactor(web): replace transition with mixin transitions
    
    * feat(web): add AppFooter component
    
    * add changeset

---
+++ .changeset/swift-parents-pull.md
@@ -0,0 +1,5 @@
+---
+"web": minor
+---
+
+feat(web): add AppFooter component
--- apps/web/src/components/AppHeader/index.tsx
+++ apps/web/src/components/AppHeader/index.tsx
@@ -1,17 +1,13 @@
 import AppButton from "@components/AppButton";
 import AppContainer from "@components/AppContainer";
+import AppLogo from "@components/AppLogo";
 import S from "./styles.module.scss";
 export default function AppHeader() {
   return (
     <header className={`${S.header}  ${S["header--sticky"]}`}>
       <AppContainer>
         <div className={S.header__content}>
-          <div className={S.logo}>
-            <div className={S.logo__icon}>
-              <span className={S.logo__icon__symbol}>⚡</span>
-            </div>
-            <span className={S.logo__text}>AutoFlow</span>
-          </div>
+          <AppLogo />
           <nav className={S.nav}>
             <AppButton>Templates</AppButton>
             <AppButton>n8n</AppButton>
