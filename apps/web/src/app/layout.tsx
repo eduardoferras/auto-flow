cdf4e61e7ab6a8dbe2a40f5577eb671dc255a835
Eduardo Ferras <edferras@gmail.com>
2025-12-25 01:27:52 -0300

    chore(web): add Space Grotesk font

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "@styles/globals.scss";
-import { inter } from "@assets/fonts";
+import { inter, spaceGrotesk } from "@assets/fonts";
 import AppHeader from "@components/AppHeader";
 import S from "./layout.module.scss";
 
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={inter.variable}>
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
         <div className={S.app}>
           <AppHeader />
           <main className={S.main}>{children}</main>
--- apps/web/src/assets/fonts.ts
+++ apps/web/src/assets/fonts.ts
@@ -1,6 +1,11 @@
-import { Inter } from "next/font/google";
+import { Inter, Space_Grotesk } from "next/font/google";
 
 export const inter = Inter({
   subsets: ["latin"],
   variable: "--font-primary",
+});
+
+export const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  variable: "--font-secondary",
 });
